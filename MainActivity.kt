b9657da6a3a4cd6f1c2cbb822d9f067c6261db20
SEPLEMBER <216141135+SEPLEMBER@users.noreply.github.com>
2026-07-20 18:13:46 +0700

    Add files via upload

---
+++ MainActivity.kt
@@ -0,0 +1,35 @@
+package es.zelliot.perceptron
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val btnMetadata = findViewById<Button>(R.id.btn_metadata)
+        val btnOrder = findViewById<Button>(R.id.btn_order)
+        val btnEdit = findViewById<Button>(R.id.btn_edit)
+        val btnSettings = findViewById<Button>(R.id.btn_settings)
+
+        btnMetadata.setOnClickListener {
+            startActivity(Intent(this, MetadataActivity::class.java))
+        }
+
+        btnOrder.setOnClickListener {
+            startActivity(Intent(this, OrderActivity::class.java))
+        }
+
+        btnEdit.setOnClickListener {
+            startActivity(Intent(this, EditorActivity::class.java))
+        }
+
+        btnSettings.setOnClickListener {
+            startActivity(Intent(this, SettingsActivity::class.java))
+        }
+    }
+}
